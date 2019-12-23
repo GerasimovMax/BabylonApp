4cbb3c93f7cd53a049dc68e6430c7b4973d17470
GerasimovMax <>
2019-12-23 16:29:59 +0300

    Added Gulp Live Reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const livereload = require('gulp-livereload');
 const tsProject = ts.createProject('tsconfig.json');
  
 gulp.task('default', function () {
   return tsProject.src()
     .pipe(ts(tsProject))
-    .js.pipe(gulp.dest('public'));
+    .js.pipe(gulp.dest('public'))
+    .pipe(livereload());
 });
 
 gulp.task('watch', function() {
+  livereload.listen();
   gulp.watch('./src/*.ts', gulp.series('default'));
 });
